3936891fe053f6cb884b8558431c8bf08d041c1c
Fernando Emilio Diaz Lopez <fernandoemiliolopez13@gmail.com>
2025-12-31 12:54:35 -0600

    ✨ Enhance zsh setup by adding powerlevel10k theme installation and updating .zshrc configurations

--- z.sh
+++ z.sh
@@ -3,13 +3,14 @@
 source ./common_functions.sh # Ensure this path is correct
 
 welcome() {
+    printf '%s\n' "                                                ";
     printf '                                   %s%s%s%s%s%s%s__   \n' $RB_RED $RB_ORANGE $RB_YELLOW $RB_GREEN $RB_YELLOW $RB_GREEN $RB_VIOLET $RB_RESET
     printf '   %s%s ____ ___  %s__  __  %s ____  %s_____%s/ /_  %s\n' $RB_RED $RB_ORANGE $RB_YELLOW $RB_GREEN $RB_YELLOW $RB_GREEN $RB_VIOLET $RB_RESET
     printf '  %s%s / __ `__ \%s/ / / / %s /_  / %s/ ___/%s __ \ %s\n' $RB_RED $RB_ORANGE $RB_YELLOW $RB_GREEN $RB_YELLOW $RB_GREEN $RB_VIOLET $RB_RESET
     printf ' %s%s / / / / / /%s /_/ / %s   / /_%s(__  )%s / / / %s\n' $RB_RED $RB_ORANGE $RB_YELLOW $RB_GREEN $RB_YELLOW $RB_GREEN $RB_VIOLET $RB_RESET
     printf '%s%s /_/ /_/ /_/%s\__, / %s   /___/%s____/%s_/ /_/  %s\n' $RB_RED $RB_ORANGE $RB_YELLOW $RB_GREEN $RB_YELLOW $RB_GREEN $RB_VIOLET $RB_RESET
     printf '          %s%s%s%s%s%s /____/%s                     %s\n' $RB_RED $RB_ORANGE $RB_YELLOW $RB_GREEN $RB_YELLOW $RB_GREEN $RB_VIOLET $RB_RESET
-    echo ""
+    printf '%s\n' "                                                ";
     echo "Make sure you have MesloLGS NF Regular Nerd Font installed and set as your terminal font."
     echo "And you're running in your desired terminal"
     sleep 5
@@ -25,12 +26,6 @@ ghostty_theme() {
     fi
 }
 
-install_zsh() {
-    sudo apt update && sudo apt upgrade -y
-    sudo apt install zsh -y
-    return 0
-}
-
 set_colors() {
     cat > ~/.dircolors <<'EOF'
     # Directories (More contrast than blue)
@@ -43,12 +38,21 @@ EOF
 return 0
 }
 
-activate_colors() {
-    cat eval "$(dircolors -b ~/.dircolors)" > ~/.zshrc
+install_zsh() {
+    sudo apt update && sudo apt upgrade -y
+    sudo apt install zsh -y
     return 0
 }
 
-set_zsh_theme() {
+setup_zsh() {
+    echo "Adding custom color configurations to .zshrc"
+    echo 'eval "$(dircolors -b ~/.dircolors)"' >> ~/.zshrc
+
+    echo "Setting default TERM to xterm-256color in .zshrc"
+    echo "export TERM=xterm-256color" >> ~/.zshrc
+
+    echo "Setting ZSH theme to powerlevel10k in .zshrc"
+
     local theme="${1:-powerlevel10k/powerlevel10k}"
     sed -i "s/^ZSH_THEME=\".*\"/ZSH_THEME=\"$theme\"/" ~/.zshrc
     echo "zsh theme changed to: $theme"
@@ -66,15 +70,20 @@ install_oh_my_zsh() {
     return 0
 }
 
+install_powerlevel10k() {
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+    return 0
+}
+
 main() {
     welcome
     ghostty_theme
-    install_zsh
     set_colors
-    activate_colors
-    set_zsh_theme
+    install_zsh
+    setup_zsh
     activate_zsh
     install_oh_my_zsh
+    install_powerlevel10k
 }
 
 # Run main
