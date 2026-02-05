dc28c1396829f240cd80408bb26d06c846b2afae
Fernando Emilio Diaz Lopez <fernandoemiliolopez13@gmail.com>
2026-02-05 15:12:36 -0600

    ♻️ Refactor of the agents_setup function in repoWizard script

--- repoWizard.sh
+++ repoWizard.sh
@@ -91,20 +91,25 @@ basic_setup() {
     git init .
 
     # Create .gitignore
-    printf '%s\n' '.env' '.venv/' 'node_modules/' '*.log' '*.tmp' '.DS_Store' CLAUDE.local.md >> .gitignore
-    print_success ".gitignore file created"
+    printf '%s\n' '.env' '.venv/' 'node_modules/' '*.log' '*.tmp' '.DS_Store' 'CLAUDE.local.md' >> .gitignore
 
     # Create basic directories
-    mkdir .vscode
+    mkdir .vscode .github .github/{workflows,ISSUE_TEMPLATE}
 
     # Create basic project files
-    touch -- .env .env.example README.md CONTRIBUTING.md LICENSE
+    touch -- .env .env.example README.md CONTRIBUTING.md LICENSE .github/workflows/sample-workflow.yml .github/dependabot.yml .github/PULL_REQUEST_TEMPLATE.md
     printf '%s\n' "# New Project" >> README.md
     printf '%s\n' "# How to contribute?" >> CONTRIBUTING.md
     printf '%s\n' "No license defined for this project yet." >> LICENSE
+
+    # Summary of created files
+    printf '%s\n' "Basic project files created:"
+    print_success ".gitignore file created"
+    print_success ".vscode directory created"
     print_success ".env and .env.example files created"
     print_success "README.md, CONTRIBUTING.md and LICENSE files created"
-    print_success "Basic project files created"
+    printf '%s\n' "Basic GitHub support added "
+    print_success ".github directory created with workflows and templates subdirectories, plus dependabot support"
 
     devcontainer_setup
     
@@ -119,31 +124,28 @@ devcontainer_setup() {
             [Yy]*|"")
                 # Create directories
                 mkdir -p docker/dev docker/prod .devcontainer
-
                 print_success "Docker and devcontainer directories created"
 
                 # Create devcontainer files
                 touch -- .devcontainer/{devcontainer.json,compose.yaml}
-
                 print_success "Devcontainers files created"
 
                 # Create docker files
                 touch docker/dev/{Dockerfile,compose.yaml} docker/prod/{Dockerfile,compose.yaml}
-
-                print_success "Dockerfiles for prod and dev created"
+                print_success "Dockerfiles for production and development environments created"
                 
                 # Create .dockerignore
-                printf '%s\n' '.git/' '.gitignore' '.gitattributes' '.devcontainer/' '.vscode/' '.venv/' '.github/' '.claude/' '.cursor/' 'node_modules/' '.env.example' '*.log' '*.tmp' '.DS_Store' README.md AGENTS.md CLAUDE.md CONTRIBUTING.md LICENSE >> .dockerignore
-                
+                printf '%s\n' '.git/' '.gitignore' '.gitattributes' '.devcontainer/' '.vscode/' '.venv/' '.github/' '.claude/' '.cursor/' '.agent/' '.opencode/' 'node_modules/' '.env.example' '*.log' '*.tmp' '.DS_Store' '.mcp.json' 'opencode.json' '.cursorindexingignore' 'README.md' 'AGENTS.md' 'CLAUDE.md' 'GEMINI.md' 'CONTRIBUTING.md' 'LICENSE' >> .dockerignore
                 print_success ".dockerignore file created"
+                
                 print_success "Devcontainer support added to the repository"
                 
-                agents_setup
+                agents_support
                 
                 return 0
                 ;;
             [Nn]*)
-                agents_setup 
+                agents_support 
                 
                 return 0
                 ;;
@@ -154,31 +156,17 @@ devcontainer_setup() {
     done
 }
 
-agents_setup() {
+agents_support() {
     while :; do
         read -erp "Create the files and directories to support AI Agents? [Y/n] " ans
 
         case "$ans" in
             [Yy]*|"")
-                mkdir -p .github .claude/rules .cursor/rules
-                touch -- AGENTS.md .claude/{CLAUDE.md,CLAUDE.local.md,rules/coding-style.md} .github/copilot-instructions.md
-                printf '%s\n' "# AGENTS.md" >> AGENTS.md
-                printf '%s\n' "# CODING STYLE.md" >> .claude/rules/coding-style.md
-
-                print_success "AGENTS.md created"
-                print_success ".claude directory created, including CLAUDE.md and rules subdirectory"
-                print_success ".github directory created, including copilot-instructions.md file"
-                print_success ".cursor directory created, including rules subdirectory"
-                print_success "AI Agents support added to the repository"
-
-
-                finish_setup
-                
+                agents_setup
                 return 0
                 ;;
             [Nn]*)
                 finish_setup
-
                 return 0
                 ;;
             *)
@@ -186,7 +174,82 @@ agents_setup() {
                 ;;
         esac
     done
+}
 
+agents_setup() {
+    printf '%s\n' "# AGENTS.md" >> AGENTS.md
+    
+    while :; do
+        printf '%s\n' "What agent do you want to support? "
+        read -erp "Type 1 for Claude, 2 for GitHub Copilot, 3 for Antigravity, 4 for Cursor, 5 for OpenCode " ans
+
+        case "$ans" in
+            1)
+                mkdir -p .claude/{agents,commands,skills/skill-example,rules}
+                touch -- CLAUDE.md CLAUDE.local.md .mcp.json
+                touch -- .claude/rules/coding-style.md
+                ln -s AGENTS.md CLAUDE.md
+
+                printf '%s\n' "# CODING STYLE.md" >> .claude/rules/coding-style.md
+
+                print_success "AGENTS.md created and linked to CLAUDE.md"
+                print_success ".claude directory created, including CLAUDE.md and rules subdirectory"
+                print_success "AI Agents support added to the repository"
+
+                finish_setup
+                
+                return 0
+                ;;
+            2)
+                mkdir -p .github/{agents,instructions,prompts,skills/SKILL-EXAMPLE}
+                touch -- .github/agents/agent-example.agent.md .github/instructions/instruction-example.instructions.md
+                touch -- .github/prompts/prompt-example.prompt.md .github/skills/SKILL-EXAMPLE/SKILL.md
+                touch -- .github/copilot-instructions.md
+                print_success "AGENTS.md and copilot-instructions.md files created"
+                print_success ".github directory created, including agents, instructions, prompt and skills directories"
+                
+                finish_setup
+                
+                return 0
+                ;;
+            3)
+                mkdir -p .agent/rules .agent/workflows .agent/skills/skill-example 
+                touch -- .agent/rules/rules.md .agent/workflows/sample-workflow.md .agent/skills/skill-example/SKILL.md
+                ln -s AGENTS.md GEMINI.md
+
+                print_success "AGENTS.md created and linked to GEMINI.md"
+                print_success ".agent directory created with rules, workflows and skills subdirectories"
+
+                finish_setup
+                
+                return 0
+                ;;
+            4)
+                mkdir -p .cursor/rules
+                touch -- .cursor/rules/coding-style.mdc .mcp.json .cursorindexingignore
+                print_success "AGENTS.md created"
+                print_success ".cursor directory created, including rules subdirectory"
+
+                finish_setup
+                
+                return 0
+                ;;
+            5)
+                mkdir -p .opencode/{agents,skill/skill-example,commands,plugins}
+                touch -- .opencode/agents/agent-example.md .opencode/skills/skill-example/SKILL.md .opencode/commands/command-example.md opencode.json
+                print_success "AGENTS.md created"
+                printf '%s\n' "OpenCode recommends to add rules for the agent in the AGENTS.md file"
+                print_success ".opencode directory created, including agents, skills, commands and plugins directories"
+
+                finish_setup
+                
+                return 0
+                ;;  
+            *)
+                print_error "Invalid input, choose between agents setup options"
+                ;;
+        esac
+    done
 }
 
 finish_setup() {
