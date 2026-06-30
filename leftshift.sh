98b74149357b01350b5e390c1997b26f52288675
Fernando <fernandoemiliolopez13@gmail.com>
2026-06-30 16:39:21 -0600

    Modificado para que el remapeo solo tenga efecto en un teclado especifico

--- leftshift.sh
+++ leftshift.sh
@@ -4,9 +4,20 @@ sudo apt update && sudo apt upgrade -y
 
 sudo apt install -y keyd
 
+# No config for all keyboards
+
 sudo tee /etc/keyd/default.conf > /dev/null <<EOF
 [ids]
 *
+
+[main]
+EOF
+
+# Config for integrated keyboard
+
+sudo tee /etc/keyd/integrated.conf > /dev/null <<EOF
+[ids]
+0001:0001:09b4e68d
 
 [main]
 leftshift = s
